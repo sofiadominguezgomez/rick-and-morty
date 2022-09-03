cc4c56bf31a7f5f1b1145b62513479336a0f1ec0
SOFIA DOMINGUEZ GOMEZ <sofiadominguezgomez03@gmail.com>
2022-09-02 23:39:43 -0300

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styles from '../styles/container.module.css'
+import github from '../media/GitHub-Mark-64px.png'
+import api_logo from '../media/rick_and_morty.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.footer_container}>
+    <div className={styles.media} >
+        <div className={styles.git} >
+            <picture> <img src={github} alt='github logo' /> </picture>
+            <h2>Done by <a href='https://github.com/sofiadominguezgomez'>Sofía Domínguez Gómez</a> </h2>
+        </div>
+        <div className={styles.api} >
+            <picture> <img src={api_logo} alt='rick and morty logo'/> </picture>
+            <h3>Consumed <a href='https://rickandmortyapi.com/'> The Rick and Morty API</a> </h3>
+        </div>
+    </div>
+        
+        <div className={styles.disclaimer}>
+            <p>
+            Rick and Morty is created by Justin Roiland and Dan Harmon for Adult Swim. The data and images are used without claim of ownership and belong to their respective owners.
+            </p>
+        </div>
+        
+        
+    </div>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,7 @@ function Header({page, setPage, totalPages}) {
     <div className={ fix ? `${styles.header_container} ${styles.fixed}` : styles.header_container}>
         <button className={styles.btn} onClick={ () => setPage(1)} disabled={ page === 1 ? true : false } > First </button>
         <button className={styles.btn} onClick={() => (page > 1) ? setPage(page-1) : null} disabled={ page === 1 ? true : false } > Previous </button>
+        <span className={styles.header_span}> {page} </span>
         <button className={styles.btn} onClick={() => setPage(page+1)} disabled={ page === totalPages ? true : false } > Next </button>
         <button className={styles.btn} onClick={() => setPage(42)} disabled={ page === totalPages ? true : false } > Last </button>
     </div>
